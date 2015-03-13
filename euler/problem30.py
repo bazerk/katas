89256b7dbf00ed072808db67f2e17507b8025037
Andrew Barrett <bazerk@gmail.com>
2015-03-13 14:04:04 +1300

    Euler problem 30 solution

--- euler/problem30.py
+++ euler/problem30.py
@@ -1,7 +1,24 @@
+import itertools
 
 
-def euler_problem30():
-    pass
+def find_max_digits():
+    nine_pow_5 = 9 ** 5
+    for digits in itertools.count(2, 1):
+        if (nine_pow_5 * digits) < int('9' * digits):
+            return digits
+
+
+def problem30():
+    max_digits = find_max_digits()
+    sum_total = 0
+    for n in xrange(11, int('9' * max_digits)):
+        power_5_sum = 0
+        for d in str(n):
+            power_5_sum += int(d) ** 5
+        if power_5_sum == n:
+            sum_total += n
+            print n
+    return sum_total
 
 if __name__ == '__main__':
-    euler_problem30()
+    print problem30()
